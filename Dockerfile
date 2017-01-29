5808e6ae12723b67a916ba0b4e9160c5deb297a8
Oleksiy Ivanenko <oleksiy.ivanenko@gmail.com>
2017-01-29 21:09:56 +0100

    Add prod like configuration

--- Dockerfile
+++ Dockerfile
@@ -20,3 +20,14 @@ ADD . /ft
 WORKDIR /ft
 
 RUN pip install -r src/.meta/packages
+
+RUN rm /etc/nginx/nginx.conf
+RUN ln -s /ft/conf/nginx/nginx.conf /etc/nginx/
+RUN ln -s /ft/conf/nginx/try.conf /etc/nginx/conf.d/
+
+RUN rm /etc/supervisord.conf
+RUN ln -s /ft/conf/supervisord/supervisord.conf /etc/
+
+EXPOSE 80 443
+
+CMD ["supervisord", "-n"]
--- src/config.py
+++ src/config.py
@@ -3,7 +3,10 @@ import os
 DEBUG = bool(os.getenv('FT_DEBUG', False))
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/fellowtraveler'
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'SQLALCHEMY_DATABASE_URI',
+    'postgresql://postgres:root@localhost/fellowtraveler'
+)
 DATABASE_CONNECT_OPTIONS = {}
 
 CSRF_ENABLED = True
